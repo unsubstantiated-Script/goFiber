ecade9f2fcf6386f0bd8fa5bac3414c68a278868
unsubstantiated-Script <jusfulton@gmail.com>
2024-06-22 18:40:44 -0700

    CMS finished

--- main.go
+++ main.go
@@ -1,3 +1,44 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"github.com/gofiber/fiber"
+	"github.com/jinzhu/gorm"
+	"goFiber/database"
+	"goFiber/lead"
+)
+
+func main() {
+	app := fiber.New()
+	initDatabase()
+	setupRoutes(app)
+	err := app.Listen("3000")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer func(DBConn *gorm.DB) {
+		err := DBConn.Close()
+		if err != nil {
+
+		}
+	}(database.DBConn)
+}
+
+func initDatabase() {
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3", "leads.db")
+	if err != nil {
+		panic("failed to connect database")
+	}
+	fmt.Println("Connected to database")
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Migrated leads")
+}
+
+func setupRoutes(app *fiber.App) {
+	app.Get("/api/v1/lead/:id", lead.GetLead)
+	app.Get("/api/v1/lead", lead.GetLeads)
+	app.Post("/api/v1/lead", lead.NewLead)
+	app.Delete("/api/v1/lead/:id", lead.DeleteLead)
+}
